4959a429debfcc13f85d8b8f41d90bae68278380
Jhaveri-Jeet <jeetwork941@gmail.com>
2024-05-20 00:05:43 +0530

    minor changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,22 +20,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-} from "@/components/ui/pagination";
-
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
 
